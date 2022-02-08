f1e6aa56a7d0a433c08ceb5e01542c41d2e6c50a
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-02-08 20:01:09 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    
+    <link rel="stylesheet" type="text/css" href="css.css" media="screen" />
+
+    
+</head>
+<body>
+    
+    <div class="inputBox1">
+        <h1>Login</h1>
+        <input type="text" placeholder="Nome de usuário">
+        <br><br>
+        <input type="password" placeholder="Senha">
+        <br><br>
+        <button>Login</button>
+        <br><br>
+        <button>Cadastre-se aqui</button>
+    </div>
+
+</body>
+</html>

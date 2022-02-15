cf300efed39f7b40129405c59b15a74bfd89223d
VladimirPinho <55743531+vladi00123@users.noreply.github.com>
2022-02-15 20:41:33 -0300

    Create paginainicial.php

---
+++ paginainicial.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>i aluno</title>
+    <style>
+        body{
+            font-family: arial, helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+        }
+       div{
+           background-color: rgb(0, 0, 0, 0,9);
+           position: absolute;
+           top: 50%;
+           left: 50%;
+           transform: translate(-50,-50);
+           padding: 80px;
+           border-radius: 15px;
+           color: #fff;
+       }
+       input{
+           padding: 15%;
+           border: none;
+           outline: none;
+           font-size: 15px;
+
+       }
+       button{
+           background-color: dodgerblue;
+           border: none;
+           padding: 15%;
+           width: 100%;
+           border-radius: 10px;
+           color: white;
+           font-size: 15px;
+       }
+       button:hover{
+           background-color: deepskyblue;
+           cursor: pointer;
+       }
+        
+    </style>
+</head>
+<body>
+    <a href="index.php">voltar</a>
+    <div>
+        <h1></h1>
+        <input type="text" placeholder="Nome">
+        <br><br>
+        <input type="password" placeholder=""senha"">
+        <br><br>
+        <button>Enviar</button>
+     </div>
+</body>
+</html>

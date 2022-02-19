5aaf335564915013f807797e1df9869a3c692010
VladimirPinho <55743531+vladi00123@users.noreply.github.com>
2022-02-19 10:10:46 -0300

    Update testdrive.php

--- testdrive.php
+++ testdrive.php
@@ -1,12 +1,41 @@
 <?php
+    session_start();
     //print_r($_REQUEST);
-    if(isset($_POST['submit']))
+    if(isset($_POST['submit']) && !empty($_POST['Nome de usuário'])) && !empty($_POST['senha'])
     {
+        //Acessa
+        include_once('conectar.php')
+        $Nome de usuário = $_POST['Nome de usuário'];
+        $senha = $_POST['senha'];
 
+       // print_r('Nome de usuário: ' . $Nome de usuário);
+       // print_r('<br>');
+       // print_r('senha: ' . $senha);
+
+       $sql="SELECT * FROM usuarios WHERE email = '$Nome de usuário' and senha = '$senha'";
+
+       $result = $conexão->query($sql);
+
+       // print_r($sql);
+       // print_r($result);
+
+       if(mysqli_num_rows($result)< 1)
+       {
+          unset($_SESSION['Nome de usuário'] = $Nome de usuário);
+          unset($_SESSION['senha'] = $senha);
+          header('location: index.php'); 
+       }
+       else
+       {
+           $_SESSION['Nome de usuário'] = $Nome de usuário;
+           $_SESSION['senha'] = $senha;
+          header('location: sistema.php');
+       }
     }
     else
     {
-        header('location:login.php');
+        //Não acessa
+        header('location:index.php');
     }
 
 ?>
